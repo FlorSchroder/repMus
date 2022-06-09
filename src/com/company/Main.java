02dc27195d6b38d0f9c3457d216ca3910d635d8e
Andres-G99 <andres_garella_99@hotmail.com>
2022-06-09 19:10:53 -0300

    RepMus 0.2.0.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import com.company.view.MarcoBusqueda;
 import com.company.view.MarcoPrincipal;
 
 import javax.swing.*;
@@ -6,7 +7,8 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        MarcoPrincipal m1 = new MarcoPrincipal();
+        //MarcoPrincipal m1 = new MarcoPrincipal();
+        MarcoBusqueda m1 = new MarcoBusqueda();
         m1.setVisible(true);
         m1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
