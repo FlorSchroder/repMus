9cb982a46a84b130bedc0a8c23a6cc223acc4f2f
Andres-G99 <andres_garella_99@hotmail.com>
2022-06-17 19:30:04 -0300

    RepMus-0.9.1

--- tp2/RepMus/src/test/java/com/company/modelo/ReproductorTest.java
+++ tp2/RepMus/src/test/java/com/company/modelo/ReproductorTest.java
@@ -1,5 +1,6 @@
 package com.company.modelo;
 
+import com.company.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReproductorTest {
     @Test
-    void name() {
-        Assertions.assertEquals(1,1);
+    void AddMeGusta() {
+        Cancion ctest = new Cancion("Holis","Musica Espantosa","Karol J", 307);
+        Lista listest = new Lista("Canciones RepMus");
+        listest.addCanciones(ctest);
+        Reproductor rep = new Reproductor();
+        rep.addLista(listest);
+        rep.getCancionesRepMus().getCanciones().get(0).setDescargado(true);
+        assertTrue(rep.getCancionesRepMus().getCanciones().get(0).isDescargado());
+
+
     }
 }
