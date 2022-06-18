9b35e79e5aff98bc1bb08b277c6e4ed083a08e65
Andres-G99 <andres_garella_99@hotmail.com>
2022-06-17 21:46:52 -0300

    repMus 0.9.3

--- src/test/java/com/company/modelo/ReproductorTest.java
+++ src/test/java/com/company/modelo/ReproductorTest.java
@@ -7,15 +7,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReproductorTest {
+    Cancion ctest = new Cancion("Holis","Musicax","Karol J", 307);
+    Lista listest = new Lista("Canciones RepMus");
+
+
     @Test
     void AddMeGusta() {
-        Cancion ctest = new Cancion("Holis","Musica Espantosa","Karol J", 307);
-        Lista listest = new Lista("Canciones RepMus");
         listest.addCanciones(ctest);
         Reproductor rep = Reproductor.getInstance();
         rep.getCancionesRepMus().getCanciones().get(0).setDescargado(true);
         assertTrue(rep.getCancionesRepMus().getCanciones().get(0).isDescargado());
 
+    }
 
+    @Test
+    void popMeGusta() {
+        listest.addCanciones(ctest);
+        Reproductor rep = Reproductor.getInstance();
+        rep.getCancionesRepMus().getCanciones().get(0).setDescargado(false);
+        assertFalse(rep.getCancionesRepMus().getCanciones().get(0).isDescargado());
+    }
+
+    @Test
+    void buscarPorGenero(){
+        listest.addCanciones(ctest);
+        Lista list2;
+        Reproductor rep = Reproductor.getInstance();
+        list2 = rep.buscar(rep.getCancionesRepMus().getCanciones().get(0).getGenero());
+        Assertions.assertEquals("Musicax",list2.getCanciones().get(0).getGenero());
     }
 }
